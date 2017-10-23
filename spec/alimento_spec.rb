32f6307ad8c852f27da99ee15b9c5042c58547f7
Victoria Quintana <alu0100889871@ull.edu.es>
2017-10-23 22:10:40 +0100

    Se crea una expectativa para almacenar y ver la existencia del nombre de un alimento rspec -I. spec/alimento_spec.rb
    Alimento
      # Existencia de la descripcion del titulo
        Se almacena correctamente el nombre del alimento (FAILED - 1)
    
    Failures:
    
      1) Alimento# Existencia de la descripcion del titulo Se almacena correctamente el nombre del alimento
         Failure/Error: expect(@food1.name).to eq("Huevo frito")
    
         NoMethodError:
           undefined method `name' for nil:NilClass
         # ./spec/alimento_spec.rb:5:in `block (3 levels) in <top (required)>'
    
    Finished in 0.00107 seconds (files took 0.0907 seconds to load)
    1 example, 1 failure
    
    Failed examples:
    
    rspec ./spec/alimento_spec.rb:4 # Alimento# Existencia de la descripcion del titulo Se almacena correctamente el nombre del alimento
    
    rake aborted!
    Command failed with status (1): [rspec -I. spec/alimento_spec.rb...]
    /home/victoria/LPP17-18/prct06/Rakefile:8:in `block in <top (required)>'
    /var/lib/gems/2.3.0/gems/rake-11.3.0/exe/rake:27:in `<top (required)>'
    Tasks: TOP => default => spec
    (See full trace by running task with --trace)

--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -1,3 +1,8 @@
 require "lib/gema/alimento.rb"
 describe Alimento do
+	describe "# Existencia de la descripcion del titulo" do
+   		it "Se almacena correctamente el nombre del alimento" do
+   			expect(@food1.name).to eq("Huevo frito")
+  		end 
+	end
 end
