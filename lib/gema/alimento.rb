da7ee18c884618eea2e1cedbdacec13f8347fd0d
Victoria Quintana <alu0100889871@ull.edu.es>
2017-10-23 23:41:01 +0100

    Arreglado la expectativa de almacenar las proteinas, los glucidos y los lipidos de un alimento rspec -I. spec/alimento_spec.rb
    Alimento
      # Existencia de las características del Alimento
        Se almacena correctamente el nombre del alimento
        Se almacena correctamente las proteínas del alimento
        Se almacena correctamente los glúcidos del alimento
        Se almacena correctamente los lípidos del alimento
    
    Finished in 0.00192 seconds (files took 0.09079 seconds to load)
    4 examples, 0 failures

--- lib/gema/alimento.rb
+++ lib/gema/alimento.rb
@@ -1,11 +1,16 @@
 class Alimento
 
-	attr_accessor :name 
-  	def initialize(name)
+	attr_accessor :name, :proteinas, :glucidos, :lipidos  
+  	def initialize(name, proteinas, glucidos, lipidos)
     		@name = name
+    		@proteinas = proteinas
+    		@glucidos = glucidos
+    		@lipidos = lipidos
  	end
+
 	def to_s
-		title= "#{@name} "
-	end
+		title= "\nAlimento:\n\n"
+		title+="#{@name}\t#{@proteinas}g\t#{@glucidos}g\t#{@lipidos}g"
+	end 
 end
 
