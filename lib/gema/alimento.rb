2332a85de281e03cca6738335eb8fa8d8b259939
Victoria Quintana <alu0100889871@ull.edu.es>
2017-10-23 23:38:28 +0100

    Se crea una expectativa para almacenar y ver las proteinas, los glucidos y los lipidos rspec -I. spec/alimento_spec.rb
    Alimento
      # Existencia de las características del Alimento
        Se almacena correctamente el nombre del alimento (FAILED - 1)
        Se almacena correctamente las proteínas del alimento (FAILED - 2)
        Se almacena correctamente los glúcidos del alimento (FAILED - 3)
        Se almacena correctamente los lípidos del alimento (FAILED - 4)
    
    Failures:
    
      1) Alimento# Existencia de las características del Alimento Se almacena correctamente el nombre del alimento
         Failure/Error:
           	def initialize(name)
             		@name = name
           end
    
         ArgumentError:
           wrong number of arguments (given 4, expected 1)
         # ./lib/gema/alimento.rb:4:in `initialize'
         # ./spec/alimento_spec.rb:5:in `new'
         # ./spec/alimento_spec.rb:5:in `block (2 levels) in <top (required)>'
    
      2) Alimento# Existencia de las características del Alimento Se almacena correctamente las proteínas del alimento
         Failure/Error:
           	def initialize(name)
             		@name = name
           end
    
         ArgumentError:
           wrong number of arguments (given 4, expected 1)
         # ./lib/gema/alimento.rb:4:in `initialize'
         # ./spec/alimento_spec.rb:5:in `new'
         # ./spec/alimento_spec.rb:5:in `block (2 levels) in <top (required)>'
    
      3) Alimento# Existencia de las características del Alimento Se almacena correctamente los glúcidos del alimento
         Failure/Error:
           	def initialize(name)
             		@name = name
           end
    
         ArgumentError:
           wrong number of arguments (given 4, expected 1)
         # ./lib/gema/alimento.rb:4:in `initialize'
         # ./spec/alimento_spec.rb:5:in `new'
         # ./spec/alimento_spec.rb:5:in `block (2 levels) in <top (required)>'
    
      4) Alimento# Existencia de las características del Alimento Se almacena correctamente los lípidos del alimento
         Failure/Error:
           	def initialize(name)
             		@name = name
           end
    
         ArgumentError:
           wrong number of arguments (given 4, expected 1)
         # ./lib/gema/alimento.rb:4:in `initialize'
         # ./spec/alimento_spec.rb:5:in `new'
         # ./spec/alimento_spec.rb:5:in `block (2 levels) in <top (required)>'
    
    Finished in 0.00154 seconds (files took 0.10808 seconds to load)
    4 examples, 4 failures
    
    Failed examples:
    
    rspec ./spec/alimento_spec.rb:29 # Alimento# Existencia de las características del Alimento Se almacena correctamente el nombre del alimento
    rspec ./spec/alimento_spec.rb:32 # Alimento# Existencia de las características del Alimento Se almacena correctamente las proteínas del alimento
    rspec ./spec/alimento_spec.rb:35 # Alimento# Existencia de las características del Alimento Se almacena correctamente los glúcidos del alimento
    rspec ./spec/alimento_spec.rb:38 # Alimento# Existencia de las características del Alimento Se almacena correctamente los lípidos del alimento
    
    rake aborted!
    Command failed with status (1): [rspec -I. spec/alimento_spec.rb...]
    /home/victoria/LPP17-18/prct06/Rakefile:8:in `block in <top (required)>'
    /var/lib/gems/2.3.0/gems/rake-11.3.0/exe/rake:27:in `<top (required)>'
    Tasks: TOP => default => spec
    (See full trace by running task with --trace)

--- lib/gema/alimento.rb
+++ lib/gema/alimento.rb
@@ -1,9 +1,11 @@
 class Alimento
-	attr_accessor :name
+
+	attr_accessor :name 
   	def initialize(name)
     		@name = name
  	end
 	def to_s
 		title= "#{@name} "
-	end 
+	end
 end
+
--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -1,11 +1,43 @@
 require "lib/gema/alimento.rb"
 describe Alimento do
-	before :each do         
-		@food1 = Alimento.new("Huevo frito")     
+	before :each do  
+       
+		@food1 = Alimento.new("Huevo frito", 14.1, 0.0, 19.5)
+		@food2 = Alimento.new("Leche vaca", 3.3, 4.8, 3.2)
+		@food3 = Alimento.new("Yogurt", 3.8, 4.9, 3.8)
+		@food4 = Alimento.new("Cerdo", 21.5, 0.0, 6.3)
+		@food5 = Alimento.new("Ternera", 21.1, 0.0, 3.1)
+		@food6 = Alimento.new("Pollo", 20.6, 0.0, 5.6)
+		@food7 = Alimento.new("Bacalao", 17.7, 0.0, 0.4)
+		@food8 = Alimento.new("Atún", 21.5, 0.0, 15.5)
+		@food9 = Alimento.new("Salmón", 19.9, 0.0, 13.6)
+		@food10 = Alimento.new("Aceite de Oliva", 0.0, 0.2, 99.6)
+		@food11 = Alimento.new("Chocolate", 5.3, 47.0, 30.0)
+		@food12 = Alimento.new("Azúcar", 5.3, 47.0, 30.0)
+		@food13 = Alimento.new("Arroz", 6.8, 77.7, 0.6)
+		@food14 = Alimento.new("Lentejas", 23.5, 52.0, 1.4)
+		@food15 = Alimento.new("Papas", 2.0, 15.4, 0.1)
+		@food16 = Alimento.new("Tomate", 1.0, 3.5, 0.2)
+		@food17 = Alimento.new("Cebolla", 1.3, 5.8, 0.3)
+		@food18 = Alimento.new("Manzana", 0.3, 12.4, 0.4)
+		@food19 = Alimento.new("Plátanos", 1.2, 21.4, 0.2)
+     
 	end
-	describe "# Existencia de la descripcion del titulo" do
+
+	describe "# Existencia de las características del Alimento" do
+
    		it "Se almacena correctamente el nombre del alimento" do
    			expect(@food1.name).to eq("Huevo frito")
   		end 
+   		it "Se almacena correctamente las proteínas del alimento" do
+   			expect(@food1.proteinas).to eq(14.1)
+  		end 
+   		it "Se almacena correctamente los glúcidos del alimento" do
+   			expect(@food1.glucidos).to eq(0.0)
+  		end 
+   		it "Se almacena correctamente los lípidos del alimento" do
+   			expect(@food1.lipidos).to eq(19.5)
+  		end 
+
 	end
 end
