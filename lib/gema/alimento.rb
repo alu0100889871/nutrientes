3d1b9dd464a06240c2eec2c5bc108afe96a52819
Victoria Quintana <alu0100889871@ull.edu.es>
2017-10-23 22:24:04 +0100

    Arreglado la expectativa de almacenar el nombre de un alimento rspec -I. spec/alimento_spec.rb
    Alimento
      # Existencia de la descripcion del titulo
        Se almacena correctamente el nombre del alimento
    
    Finished in 0.00131 seconds (files took 0.10844 seconds to load)
    1 example, 0 failures

--- lib/gema/alimento.rb
+++ lib/gema/alimento.rb
@@ -1,2 +1,9 @@
 class Alimento
+	attr_accessor :name
+  	def initialize(name)
+    		@name = name
+ 	end
+	def to_s
+		title= "#{@name} "
+	end 
 end
--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -1,5 +1,8 @@
 require "lib/gema/alimento.rb"
 describe Alimento do
+	before :each do         
+		@food1 = Alimento.new("Huevo frito")     
+	end
 	describe "# Existencia de la descripcion del titulo" do
    		it "Se almacena correctamente el nombre del alimento" do
    			expect(@food1.name).to eq("Huevo frito")
