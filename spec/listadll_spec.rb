75b7df3ee85ac7c53a69a1b0e3fc7fcfe369d5ed
Victoria Quintana <alu0100889871@ull.edu.es>
2017-11-06 23:36:05 +0000

    3º Expectativa del TDD: inicialización del nodo, da el siguiente error: rspec -I. spec/listadll_spec.rb
    Listadll
      Nododll
        #Debe existir un Nodo de la lista doblemente enlazada y el siguiente. Nodo inicializado correctamente (FAILED - 1)
    
    Failures:
    
      1) Listadll Nododll #Debe existir un Nodo de la lista doblemente enlazada y el siguiente. Nodo inicializado correctamente
         Failure/Error: @nod = Nodedll.new(12,nil,nil)
    
         NameError:
           uninitialized constant Nodedll
         # ./spec/listadll_spec.rb:7:in `block (2 levels) in <top (required)>'
    
    Finished in 0.0009 seconds (files took 0.09036 seconds to load)
    1 example, 1 failure
    
    Failed examples:
    
    rspec ./spec/listadll_spec.rb:12 # Listadll Nododll #Debe existir un Nodo de la lista doblemente enlazada y el siguiente. Nodo inicializado correctamente
    
    rake aborted!
    Command failed with status (1): [rspec -I. spec/listadll_spec.rb...]
    /home/victoria/LPP17-18/prct06/Rakefile:13:in `block in <top (required)>'
    /var/lib/gems/2.3.0/gems/rake-11.3.0/exe/rake:27:in `<top (required)>'
    Tasks: TOP => default => spec
    (See full trace by running task with --trace)

--- spec/listadll_spec.rb
+++ spec/listadll_spec.rb
@@ -2,4 +2,18 @@ require "lib/gema/alimento.rb"
 require "lib/gema/listadll.rb"
 
 describe Listadll do
+
+	before :all do
+		@nod = Nodedll.new(12,nil,nil)
+		@lista = Listadll.new()
+	end
+
+	describe "Nododll" do
+     		it "#Debe existir un Nodo de la lista doblemente enlazada y el siguiente. Nodo inicializado correctamente" do
+     			expect(@nod.value).to eq(12)
+      			expect(@nod.prev).to eq(nil)
+			expect(@nod.next).to eq(nil)
+			
+        	end
+	end
 end
