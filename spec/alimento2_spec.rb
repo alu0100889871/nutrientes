c77af6fc3f093d6e6e5d026d6402e80449122bd1
Victoria Quintana <alu0100889871@ull.edu.es>
2017-11-22 00:26:07 +0000

    Expectativa de indice glucemico de yogurt

--- spec/alimento2_spec.rb
+++ spec/alimento2_spec.rb
@@ -1,9 +1,7 @@
 require "lib/gema/alimento2.rb"
 describe Alimento2 do 
 	before :each do 
-		@glucosa1 = [4.9, 5.3, 5.9, 6.7, 7.2, 7.6, 8.0, 8.2, 8.2, 8.4, 8.3, 8.3, 8.0, 7.5, 7.1, 6.8, 6.8, 6.9, 6.8, 6.3, 6.2, 6.3, 6.2, 6.3, 6.1]
-		@glucosa2 = [6.3, 5.4, 5.6, 5.7, 6.5, 7.4, 7.9, 7.4, 7.7, 7.9, 7.9, 7.8, 7.8, 7.8, 8.0, 8.5, 9.4, 10.8, 10.5, 9.1, 8.9, 8.3, 7.7, 7.6, 7.5]
-
+		@glucosa = [[4.9, 5.3, 5.9, 6.7, 7.2, 7.6, 8.0, 8.2, 8.2, 8.4, 8.3, 8.3, 8.0, 7.5, 7.1, 6.8, 6.8, 6.9, 6.8, 6.3, 6.2, 6.3, 6.2, 6.3, 6.1],[6.3, 5.4, 5.6, 5.7, 6.5, 7.4, 7.9, 7.4, 7.7, 7.9, 7.9, 7.8, 7.8, 7.8, 8.0, 8.5, 9.4, 10.8, 10.5, 9.1, 8.9, 8.3, 7.7, 7.6, 7.5]]
 		@alimento1_1 = Alimento2.new("Compota de manzana",14.1, 0.0, 19.5,[6.7, 6.5, 6.8, 6.9, 7.0, 7.1, 6.9, 6.9, 6.9, 6.7, 6.9, 7.3, 7.0, 7.0, 7.2, 7.1, 6.8, 7.2, 7.3, 7.0, 6.8, 6.7, 6.8, 6.7, 6.9])
 		@alimento1_2 = Alimento2.new("Yogurt",14.1, 0.0, 19.5,[6.1, 6.6, 6.3, 6.3, 6.1, 6.9, 6.8, 6.5, 6.4, 6.9, 6.8, 6.5, 6.3, 6.2, 6.7, 6.2, 5.9, 5.8, 5.8, 5.8, 5.8, 5.8, 5.9, 6.2, 6.4])
 		@alimento1_3 = Alimento2.new("Chocolate",3.8, 4.9, 3.8,[6.5, 6.5, 6.7, 6.5, 6.5, 6.8, 6.7, 6.2, 6.5, 7.2, 6.9, 7.0, 6.3, 6.2, 6.1, 5.9, 5.8, 6.1, 6.7, 6.7, 6.6, 6.7, 6.9, 7.2, 7.1])
@@ -18,14 +16,21 @@ describe Alimento2 do
    			expect(@individuos[0][0].aibc_alim).to eq(28.249999999999993)
   		end 
    		it "#Se realiza correctamente el aibc_glucosa" do
-   			expect(@individuos[0][0].aibc_glucosa(@glucosa1)).to eq(251.5)
+   			expect(@individuos[0][0].aibc_glucosa(@glucosa[0])).to eq(251.5)
   		end 
    		it "#Se realiza correctamente el indice glucemico de Compota de manzana" do
-			div = (0...@individuos.length).map{|i| @individuos[i][0].aibc_alim/@individuos[i][0].aibc_glucosa(@glucosa1)}
+			div = (0...@individuos.length).map{|i| @individuos[i][0].aibc_alim/@individuos[i][0].aibc_glucosa(@glucosa[i])}
 			mult = (0...div.length).map{|i| div[i]*100}
 			div2 = (0...mult.length).map{|i| mult[i]/2}
 			manzana = div2.reduce('+')
-			expect(manzana).to eq(41.50099403578528)	
+			expect(manzana).to eq(52.438610876891694)	
+  		end
+   		it "#Se realiza correctamente el indice glucemico de Yogurt" do
+			div = (0...@individuos.length).map{|i| @individuos[i][1].aibc_alim/@individuos[i][1].aibc_glucosa(@glucosa[i])}
+			mult = (0...div.length).map{|i| div[i]*100}
+			div2 = (0...mult.length).map{|i| mult[i]/2}
+			yogurt = div2.reduce('+')
+			expect(yogurt).to eq(39.46044872141986)	
   		end
 	
 	end
