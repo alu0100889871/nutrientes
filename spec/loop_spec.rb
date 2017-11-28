cc5b22673c2982b1a684c3f2096c9fa5952a387e
Victoria Quintana <alu0100889871@ull.edu.es>
2017-11-28 22:27:57 +0000

    Creada la expectativa del for rspec -I. spec/loop_spec.rb
    Alimento
      For, each y sort
        #Se realiza correctamente el for (FAILED - 1)
    
    Failures:
    
      1) Alimento For, each y sort #Se realiza correctamente el for
         Failure/Error: expect(aux).to eq([@food16,@food21, @food17, @food18, @food22, @food2, @food3, @food15, @food7, @food19, @food5, @food6, @food4, @food9, @food8, @food1, @food14, @food13, @food11, @food12, @food20, @food10])
    
         NameError:
           undefined local variable or method `aux' for #<RSpec::ExampleGroups::Alimento::ForEachYSort:0x000000024734f0>
         # ./spec/loop_spec.rb:32:in `block (3 levels) in <top (required)>'
    
    Finished in 0.00098 seconds (files took 0.09447 seconds to load)
    1 example, 1 failure
    
    Failed examples:
    
    rspec ./spec/loop_spec.rb:31 # Alimento For, each y sort #Se realiza correctamente el for
    
    rake aborted!
    Command failed with status (1): [rspec -I. spec/loop_spec.rb...]
    /home/victoria/LPP17-18/practicaLPP/Rakefile:27:in `block in <top (required)>'
    /var/lib/gems/2.3.0/gems/rake-11.3.0/exe/rake:27:in `<top (required)>'
    Tasks: TOP => default => spec
    (See full trace by running task with --trace)

--- spec/loop_spec.rb
+++ spec/loop_spec.rb
@@ -1,1 +1,35 @@
 require "lib/gema/alimento.rb"
+
+describe Alimento do
+	before :all do      
+		@food1 = Alimento.new("Huevo frito", 14.1, 0.0, 19.5)
+		@food2 = Alimento.new("Leche vaca", 3.3, 4.8, 3.2)
+		@food3 = Alimento.new("Yogurt", 3.8, 4.9, 3.8)
+		@food4 = Alimento.new("Cerdo", 21.5, 0.0, 6.3)
+		@food5 = Alimento.new("Ternera", 21.1, 0.0, 3.1)
+		@food6 = Alimento.new("Pollo", 20.6, 0.0, 5.6)
+		@food7 = Alimento.new("Bacalao", 17.7, 0.0, 0.4)
+		@food8 = Alimento.new("Atún", 21.5, 0.0, 15.5)
+		@food9 = Alimento.new("Salmón", 19.9, 0.0, 13.6)
+		@food10 = Alimento.new("Aceite de Oliva", 0.0, 0.2, 99.6)
+		@food11 = Alimento.new("Chocolate", 5.3, 47.0, 30.0)
+		@food12 = Alimento.new("Azúcar", 5.3, 47.0, 30.0)
+		@food13 = Alimento.new("Arroz", 6.8, 77.7, 0.6)
+		@food14 = Alimento.new("Lentejas", 23.5, 52.0, 1.4)
+		@food15 = Alimento.new("Papas", 2.0, 15.4, 0.1)
+		@food16 = Alimento.new("Tomate", 1.0, 3.5, 0.2)
+		@food17 = Alimento.new("Cebolla", 1.3, 5.8, 0.3)
+		@food18 = Alimento.new("Manzana", 0.3, 12.4, 0.4)
+		@food19 = Alimento.new("Plátanos", 1.2, 21.4, 0.2)
+		@food20 = Alimento.new("Mantequilla", 0.7, 0.0, 83.2)
+		@food21 = Alimento.new("Calabaza", 1.1, 4.8, 0.1)
+		@food22 = Alimento.new("Pera", 0.5, 12.7, 0.3)
+        	@array = [@food1, @food2, @food3, @food4, @food5, @food6, @food7, @food8, @food9, @food10, @food11, @food12, @food13, @food14, @food15, @food16, @food17, @food18, @food19, @food20, @food21, @food22]
+		
+	end
+	describe "For, each y sort" do
+   		it "#Se realiza correctamente el for" do
+			expect(aux).to eq([@food16,@food21, @food17, @food18, @food22, @food2, @food3, @food15, @food7, @food19, @food5, @food6, @food4, @food9, @food8, @food1, @food14, @food13, @food11, @food12, @food20, @food10])	
+		end
+	end
+end
